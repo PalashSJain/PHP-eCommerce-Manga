82841bceb7e09a4f35adb1a38e87b2d25f45c3be
PalashSJain <palash.jain@mail.rit.edu>
2018-02-23 00:48:49 -0500

    Created Headers by calling getHeader function from index, cart and admin php files

--- php/index.php
+++ php/index.php
@@ -5,3 +5,16 @@
  * Date: 2/22/2018
  * Time: 10:11 PM
  */
+
+include "utils/LIB_project1.php" ?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<?php
+echo getHeader("Home");
+?>
+</body>
+</html>
--- php/utils/LIB_project1.php
+++ php/utils/LIB_project1.php
@@ -5,3 +5,25 @@
  * Date: 2/22/2018
  * Time: 10:12 PM
  */
+
+/**
+ * @param $currentPage: Can be either "Home", "Cart", "Admin". Helps set class active on header links
+ * @return string: HTML UI element of the Header Navigation System
+ */
+function getHeader($currentPage)
+{
+    return "<nav class=\"navbar navbar-default\">
+                <div class=\"container-fluid\">
+                    <div class=\"navbar-header\">
+                        <a class=\"navbar-brand\" href=\"#\">Manga Shop</a>
+                    </div>
+                    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+                        <ul class=\"nav navbar-nav navbar-right\">
+                            <li class=" . ($currentPage == "Home" ? "\"active\"" : "") . "><a href=\"/PHP-eCommerce-Manga/php/index.php\">Home</a></li>
+                            <li class=" . ($currentPage == "Cart" ? "\"active\"" : "") . "><a href=\"/PHP-eCommerce-Manga/php/cart/cart.php\">Cart</a></li>
+                            <li class=" . ($currentPage == "Admin" ? "\"active\"" : "") . "><a href=\"/PHP-eCommerce-Manga/php/admin/admin.php\">Admin</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>";
+}
