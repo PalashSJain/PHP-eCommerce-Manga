781286f6edcd913e3fc3f051d82ea646fbde6355
PalashSJain <palash.jain@mail.rit.edu>
2018-02-23 00:55:54 -0500

    Replaced bootstrap cdn with local files

--- php/cart/cart.php
+++ php/cart/cart.php
@@ -5,10 +5,10 @@
  * Date: 2/22/2018
  * Time: 10:11 PM
  */
-include "../utils/LIB_project1.php" ?>
+include $_SERVER['DOCUMENT_ROOT']."/php/utils/LIB_project1.php" ?>
 <html>
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    <link rel="stylesheet" href="/PHP-eCommerce-Manga/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
