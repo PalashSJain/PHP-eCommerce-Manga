784ed37bbe8952294c38052ef2f9b9aa6ed8e4fb
PalashSJain <palash.jain@mail.rit.edu>
2018-03-15 16:43:59 -0400

    Enveloped DB calls into try-catch

--- php/db/DB.MangaStore.class.php
+++ php/db/DB.MangaStore.class.php
@@ -27,161 +27,203 @@ class dbMangaStore
 
     public function getProductsOnSale()
     {
-        $query = "SELECT * FROM products WHERE salePrice != 0";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
+        try {
+            $query = "SELECT * FROM products WHERE salePrice != 0";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
 
-        return $stmt->fetchAll();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getProductsInCatalog($limit, $offset)
     {
-        $query = "SELECT * FROM products WHERE salePrice = 0 LIMIT $limit OFFSET $offset";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
+        try {
+            $query = "SELECT * FROM products WHERE salePrice = 0 LIMIT $limit OFFSET $offset";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
 
-        return $stmt->fetchAll();
-    }
-
-    public function insert($title, $description, $price, $quantity, $imageName, $salePrice)
-    {
-        $query = "INSERT INTO 
-                    products (productName, description, price, quantity, imageName, salePrice) 
-                    VALUES (:title, :description, :price, :quantity, :imageName, :salePrice)";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(':title' => $title,
-            ':description' => $description,
-            ':price' => $price,
-            ':quantity' => $quantity,
-            ':imageName' => $imageName,
-            ':salePrice' => $salePrice));
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getUser($userID)
     {
-        $query = "SELECT * FROM users WHERE username = :userID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':userID' => $userID));
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "User");
-        return $stmt->fetch();
+        try {
+            $query = "SELECT * FROM users WHERE username = :userID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':userID' => $userID));
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "User");
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getCartItem($productId, $sid)
     {
-        $query = "SELECT * FROM carts WHERE productID = :productID AND sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sid,
-            ':productID' => $productId
-        ));
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "Cart");
-        return $stmt->fetch();
+        try {
+            $query = "SELECT * FROM carts WHERE productID = :productID AND sessionID = :sessionID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sid,
+                ':productID' => $productId
+            ));
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "Cart");
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function updateQuantityInCart($productId, $sid)
     {
-        $query = "UPDATE carts SET quantity = quantity + 1 WHERE sessionID = :sessionID AND productID = :productID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sid,
-            ':productID' => $productId
-        ));
-        return $stmt->rowCount();
+        try {
+            $query = "UPDATE carts SET quantity = quantity + 1 WHERE sessionID = :sessionID AND productID = :productID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sid,
+                ':productID' => $productId
+            ));
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function insertItemToCart($productId, $sid)
     {
-        $query = "INSERT INTO carts (sessionID, productID, quantity) VALUES (:sessionID, :productID, 1)";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sid,
-            ':productID' => $productId
-        ));
-        return $this->pdo->lastInsertId();
+        try {
+            $query = "INSERT INTO carts (sessionID, productID, quantity) VALUES (:sessionID, :productID, 1)";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sid,
+                ':productID' => $productId
+            ));
+            return $this->pdo->lastInsertId();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function replaceCartWithNewSessionID($oldID, $newID)
     {
-        $query = "UPDATE carts SET sessionID = :newID WHERE sessionID = :oldID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':newID' => $newID,
-            ':oldID' => $oldID
-        ));
+        try {
+            $query = "UPDATE carts SET sessionID = :newID WHERE sessionID = :oldID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':newID' => $newID,
+                ':oldID' => $oldID
+            ));
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getNumberOfProductsInCart($sessionID)
     {
-        $query = "SELECT count(*) AS no_of_products FROM carts WHERE sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sessionID));
-        $data = $stmt->fetch();
-        return $data["no_of_products"];
+        try {
+            $query = "SELECT count(*) AS no_of_products FROM carts WHERE sessionID = :sessionID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sessionID));
+            $data = $stmt->fetch();
+            return $data["no_of_products"];
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getProductsInCart($sessionID)
     {
-        $query = "SELECT c.quantity AS quantity, p.productName AS title, p.price AS price, p.description AS description, p.salePrice AS salePrice,
+        try {
+            $query = "SELECT c.quantity AS quantity, p.productName AS title, p.price AS price, p.description AS description, p.salePrice AS salePrice,
  p.imageName AS imageName FROM carts c INNER JOIN products p ON c.productID = p.productID WHERE sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sessionID));
-        return $stmt->fetchAll();
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sessionID));
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getCartTotal($sessionID)
     {
-        $query = "SELECT sum(p.price) AS total FROM carts c INNER JOIN products p ON c.productID = p.productID WHERE c.sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sessionID));
-        $data = $stmt->fetch();
-        return $data["total"];
+        try {
+            $query = "SELECT sum(p.price) AS total FROM carts c INNER JOIN products p ON c.productID = p.productID WHERE c.sessionID = :sessionID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sessionID));
+            $data = $stmt->fetch();
+            return $data["total"];
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function refillProductsQuantityFromCart($sessionID)
     {
-        $query = "UPDATE products 
+        try {
+            $query = "UPDATE products 
             JOIN carts ON carts.productID = products.productID 
             SET products.quantity = products.quantity + carts.quantity
             WHERE carts.sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sessionID
-        ));
 
-        return $stmt->rowCount();
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sessionID
+            ));
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function removeProductsFromCart($sessionID)
     {
-        $query = "DELETE FROM carts WHERE sessionID = :sessionID";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':sessionID' => $sessionID
-        ));
+        try {
+            $query = "DELETE FROM carts WHERE sessionID = :sessionID";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':sessionID' => $sessionID
+            ));
 
-        return $stmt->rowCount();
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getNumberOfProductsInCatalog()
     {
-        $query = "SELECT count(*) AS no_of_products FROM products WHERE salePrice = 0";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        return intval($stmt->fetch()['no_of_products']);
+        try {
+            $query = "SELECT count(*) AS no_of_products FROM products WHERE salePrice = 0";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            return intval($stmt->fetch()['no_of_products']);
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function addProduct($name, $description, $file, $quantity, $price, $salePrice)
     {
-        $query = "INSERT INTO products (productName, description, imageName, quantity, price, salePrice) 
-            VALUES (:name, :description, :file, :quantity, :price, :salePrice)";
-        $stmt = $this->pdo->prepare($query);
         try {
+            $query = "INSERT INTO products (productName, description, imageName, quantity, price, salePrice) 
+            VALUES (:name, :description, :file, :quantity, :price, :salePrice)";
+
+            $stmt = $this->pdo->prepare($query);
             $stmt->execute(array(
                 ':name' => $name,
                 ':description' => $description,
@@ -190,85 +232,115 @@ class dbMangaStore
                 ':price' => $price,
                 'salePrice' => $salePrice
             ));
-        } catch (Exception $e) {
-            return "Failed to add '$name'.";
+            return "'$name' added successfully.";
+        } catch (PDOException $e) {
+            die();
         }
-        return "'$name' added successfully.";
     }
 
     public function getNumberOfProductsWithName($name)
     {
-        $query = "SELECT count(*) AS no_of_products FROM products WHERE productName = :name";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(':name' => $name));
-        return $stmt->fetch()['no_of_products'];
+        try {
+            $query = "SELECT count(*) AS no_of_products FROM products WHERE productName = :name";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(':name' => $name));
+            return $stmt->fetch()['no_of_products'];
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getNumberOfProductsOnSale()
     {
-        $query = "SELECT count(*) AS no_of_products FROM products WHERE salePrice != 0";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array());
-        return $stmt->fetch()['no_of_products'];
+        try {
+            $query = "SELECT count(*) AS no_of_products FROM products WHERE salePrice != 0";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array());
+            return $stmt->fetch()['no_of_products'];
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getAllProductNames()
     {
-        $query = "SELECT productName FROM products ORDER by productID DESC";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
+        try {
+            $query = "SELECT productName FROM products ORDER BY productID DESC";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
 
-        return $stmt->fetchAll();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function getProductFromName($name)
     {
-        $query = "SELECT * FROM products WHERE productName = :name";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(':name' => $name));
-        $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
+        try {
+            $query = "SELECT * FROM products WHERE productName = :name";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(':name' => $name));
+            $stmt->setFetchMode(PDO::FETCH_CLASS, "Product");
 
-        return $stmt->fetch();
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function updateProductInformation($oldProductName, $newName, $newDescription, $newImage, $newQuantity, $newPrice, $newSalePrice)
     {
-        $query = "UPDATE products SET productName = :newName, description = :newDescription, imageName=:newImage, 
+        try {
+            $query = "UPDATE products SET productName = :newName, description = :newDescription, imageName=:newImage, 
               quantity = :newQuantity, price = :newPrice, salePrice = :newSalePrice WHERE productName = :oldProductName";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':newName' => $newName,
-            ':newDescription' => $newDescription,
-            ':newImage' => $newImage,
-            ':newQuantity' => $newQuantity,
-            ':newPrice' => $newPrice,
-            ':newSalePrice' => $newSalePrice,
-            ':oldProductName' => $oldProductName,
-        ));
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':newName' => $newName,
+                ':newDescription' => $newDescription,
+                ':newImage' => $newImage,
+                ':newQuantity' => $newQuantity,
+                ':newPrice' => $newPrice,
+                ':newSalePrice' => $newSalePrice,
+                ':oldProductName' => $oldProductName,
+            ));
 
-        return $stmt->rowCount();
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function reduceQuantity($productId)
     {
-        $query = "UPDATE products SET quantity = quantity - 1 WHERE productID = :productId";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':productId' => $productId
-        ));
-        return $stmt->rowCount();
+        try {
+            $query = "UPDATE products SET quantity = quantity - 1 WHERE productID = :productId";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':productId' => $productId
+            ));
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
     public function addUser($username, $password, $role)
     {
-        $query = "INSERT INTO users(username, password, role) VALUES (:username, :password, :role)";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute(array(
-            ':username' => $username,
-            ':password' => password_hash($password, PASSWORD_BCRYPT),
-            ':role' => $role
-        ));
+        try {
+            $query = "INSERT INTO users(username, password, role) VALUES (:username, :password, :role)";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(array(
+                ':username' => $username,
+                ':password' => password_hash($password, PASSWORD_BCRYPT),
+                ':role' => $role
+            ));
+
+            return $this->pdo->lastInsertId();
+        } catch (PDOException $e) {
+            die();
+        }
     }
 
 }
@@ -286,7 +358,7 @@ class dbMangaStore
 //    $imageName = "/PHP-eCommerce-Manga/images/$count.jpg";
 //    $salePrice = 0;
 //
-//    $db->insert($title, $description, $price, $quantity, $imageName, $salePrice);
+//    $db->addProduct($title, $description, $price, $quantity, $imageName, $salePrice);
 //    $count += 1;
 //
 //    if ($count > 43) break;
